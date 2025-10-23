580c14e6d566ddefcdbd957c3b65a280dcdd787a
HerrecuDev <phercue2805@g.educaand.es>
2025-10-23 13:03:23 +0200

    proyecto bien creado

--- src/main/java/org/iesdm_demoph2025/sprintboot_demo_2025/model/Cliente.java
+++ src/main/java/org/iesdm_demoph2025/sprintboot_demo_2025/model/Cliente.java
@@ -2,7 +2,7 @@ package org.iesdm_demoph2025.sprintboot_demo_2025.model;
 
 import lombok.Data;
 @Data
-public class cliente {
+public class Cliente {
     private int id;
     private String nombre;
     private String apellido1;
